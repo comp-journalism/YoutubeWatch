06c7ae62b9cd5d2fa57cbc842a64bac00cd9e4f0
Jack Bandy <jackbandy@u.northwestern.edu>
2022-07-25 13:13:09 -0500

    Tweak to Intro

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -10,13 +10,7 @@ export default function Intro() {
         </h1>
         <h5 className="text-2xl pb-10">
 
-        In 2017, YouTube's product chief shared that 70% of time spent on YouTube is driven by algorithmic recommendations.
-
-        81% of U.S. adults use YouTube, according to <a href="https://www.pewresearch.org/internet/2021/04/07/social-media-use-in-2021/">Pew Research.</a>
-
-        The average user spends about 30 m inutes per visit, according to <a href="https://www.statista.com/statistics/1257254/youtubecom-time-spent-per-visit/">Statista</a>
-
-        This dashboard aims to help answer the question - what is everybody watching? That is, what is YouTube's algorithm showing everybody?
+        In 2017, YouTube's product chief <a href="https://www.cnet.com/tech/services-and-software/youtube-ces-2018-neal-mohan/">shared</a> that 70% of time spent on YouTube is driven by algorithmic recommendations. This dashboard aims to help answer the question - what is YouTube's algorithm showing everybody?
             
         </h5>
 
